ff0659b1f189645e1e9767088cae54abe80d0fb7
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-10-09 15:56:18 +0800

    revised

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const clickRandomMovement = document.querySelector('.click-random-movement')
+let move = 90
+let secondMove = 90
+
+const randomNumber = () => {
+  return Math.ceil(Math.random() * 2)
+}
 
 clickRandomMovement.addEventListener('click', () => {
   clickRandomMovement.style.position = 'absolute'
-  clickRandomMovement.style.top = `${Math.random() * 99}%`
-  clickRandomMovement.style.left = `${Math.random() * 99}%`
+  let randomMove = () => {
+    if (move == 90) {
+      move = 0
+      secondMove = 90
+    } else {
+      move = 90
+      secondMove = 0
+    }
+  }
+
+  randomMove()
+  clickRandomMovement.style.left = `${randomNumber() == 1 ? move : secondMove}%`
+  clickRandomMovement.style.top = `${randomNumber() == 2 ? move : secondMove}%`
 })
